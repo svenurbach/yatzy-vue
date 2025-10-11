d182a96ad30aee2e98d6ec6bf645a24f762a225e
Sven Urbach <urbachsven@gmail.com>
2025-10-11 17:04:51 +0200

    logic

--- src/stores/game.ts
+++ src/stores/game.ts
@@ -1,28 +1,104 @@
-import { ref, computed } from 'vue'
+import { ref, computed, readonly } from 'vue'
 import { defineStore } from 'pinia'
-import type { Player} from '@/types/player'
+import type { Player } from '@/types/player'
+import { useDiceSet } from '@/composables/useDiceSet'
 
 export const useGameStore = defineStore('game', () => {
 	//  States / Data
-	const playerCount = ref(1)
-	const player = ref<Player[]>()
+	const diceSet = useDiceSet(5)
+	const rollsLeft = ref(3)
+	const hasDecision = ref(false)
+	const playerCount = ref(0)
+	const MAX_PLAYER = 3;
+	const players = ref<Player[]>([])
+	const currentPlayerId = ref(0)
+	// Views
+	const welcomeViewActive = ref(true)
+	const boardViewActive = ref(false)
 
 	// Getters / Properties
-	const doubleCount = computed(() => playerCount.value * 2)
+	const getCurrentPlayer = computed(() => players.value.find(p => p.id === currentPlayerId.value))
+	const getDiceSet = computed(() => diceSet.diceSet.map(dice => dice.face))
 
-	// Actions / Methods
-	function increment() {
-		playerCount.value++
+
+	function getPlayerByID(playerId: number): Player | undefined {
+		const player = players.value.find(p => p.id === playerId)
+		if (!player) {
+			console.warn(`Spieler mit ID ${playerId} nicht gefunden.`)
+		}
+		return player
 	}
 
-	function changePlayer() {
-		console.log("ChangePlayer");
-	}
-
-	function newGame() {
-		console.log("startNewGame");
+	// Actions (Setter) / Methods
+	function setPlayerCount(count: number): void {
+		playerCount.value = count;
+		console.log("playerCount.value", playerCount.value);
 
 	}
 
-	return { playerCount, doubleCount, player, increment }
+	function setCurrentPlayerId(playerId: number): void {
+		currentPlayerId.value = playerId;
+		console.log("currentPlayerId changed", currentPlayerId.value);
+
+	}
+
+	function clearPlayers(): void {
+		players.value = [];
+	}
+
+	function setCategoryScore(playerId: number, category: keyof Player, score: number) {
+		const player = getPlayerByID(playerId)
+		if (player && player[category] === null) {
+			player[category] = score
+		}
+	}
+
+	function setPlayers(count: number): void {
+		clearPlayers();
+		for (let i = 0; i < count; i++) {
+			const newPlayer: Player = {
+				id: i + 1,
+				totalScore: 0,
+				upperSectionScore: 0,
+				lowerSectionScore: 0,
+				aces: null,
+				twos: null,
+				threes: null,
+				fours: null,
+				fives: null,
+				sixes: null,
+				threeOfKind: null,
+				fourOfKind: null,
+				fullHouse: null,
+				smallStraight: null,
+				largeStraight: null,
+				chance: null,
+				yatzy: null
+			}
+			players.value.push(newPlayer);
+		}
+		console.log("setPlayers", players.value)
+	}
+
+	function resetRolls(): void {
+		rollsLeft.value = 3;
+	}
+
+	return {
+		MAX_PLAYER,
+		diceSet,
+		getDiceSet,
+		rollsLeft,
+		resetRolls,
+		hasDecision,
+		setPlayerCount,
+		setPlayers,
+		setCurrentPlayerId,
+		setCategoryScore,
+		currentPlayerId,
+		playerCount: readonly(playerCount),
+		welcomeViewActive,
+		boardViewActive,
+		getCurrentPlayer,
+	}
 })
--- src/types/player.ts
+++ src/types/player.ts
@@ -1,18 +1,19 @@
 export interface Player {
-  totalScore: number;
-  upperSectionScore: number;
-  lowerSectionScore: number;
-  aces: null | number;
-  twos: null | number;
-  threes: null | number;
-  fours: null | number;
-  fives: null | number;
-  sixes: null | number;
-  threeOfKind: null | number;
-  fourOfKind: null | number;
-  fullHouse: null | number;
-  smallStraight: null | number;
-  largeStraight: null | number;
-  chance: null | number;
-  yatzy: null | number;
+	id: number,
+	totalScore: number;
+	upperSectionScore: number;
+	lowerSectionScore: number;
+	aces: null | number;
+	twos: null | number;
+	threes: null | number;
+	fours: null | number;
+	fives: null | number;
+	sixes: null | number;
+	threeOfKind: null | number;
+	fourOfKind: null | number;
+	fullHouse: null | number;
+	smallStraight: null | number;
+	largeStraight: null | number;
+	chance: null | number;
+	yatzy: null | number;
 }
